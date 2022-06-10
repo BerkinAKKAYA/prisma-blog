917f3fd1ebd720363ef5922ff05a53c4a965e003
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2022-06-10 10:23:10 +0300

    return after res.json

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,7 +7,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'POST') {
 		const data = JSON.parse(req.body);
 		const response = await prisma.post.create({ data });
-		res.json(response);
+		return res.json(response);
 	}
 
 	if (req.method === 'PATCH') {
@@ -16,13 +16,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			where: { id: data.id },
 			data: data.data
 		});
-		res.json(response);
+		return res.json(response);
 	}
 
 	if (req.method === 'DELETE') {
 		const data: { id: number } = JSON.parse(req.body);
 		const response = await prisma.post.delete({ where: { id: data.id } });
-		res.json(response);
+		return res.json(response);
 	}
 
 	return res.status(405).json({ message: 'Method Not Allowed' });
